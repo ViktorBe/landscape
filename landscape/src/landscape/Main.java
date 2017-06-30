2d990b930b9ee8a82a071ecc07423287beea2ce0
DESKTOP-HIN6K63\VB <aiden_vb@yahoo.de>
2017-06-30 13:06:12 +0200

    Add a greeting string

--- landscape/src/landscape/Main.java
+++ landscape/src/landscape/Main.java
@@ -3,6 +3,7 @@ package landscape;
 public class Main {
 
 	public static void main(String[] args) {
+		System.out.println("Let the landscape grow!");
 		Mountain f = new Mountain("Fudji", 3005);
 		int[] a = {6,1,2,3,9,};
 		int[] b = {1,2,4,4,7,6};
